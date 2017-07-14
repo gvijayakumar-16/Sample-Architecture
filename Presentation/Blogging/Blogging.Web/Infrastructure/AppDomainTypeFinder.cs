b1cbf55d90f169f9a57dcb98c89da997ee1da460
Vijayakumar <gvijayakumar.ganesh@gmail.com>
2017-07-14 18:43:51 +0530

    Code refactored

---
+++ Presentation/Blogging/Blogging.Web/Infrastructure/AppDomainTypeFinder.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Blogging.Web.Infrastructure
+{
+    public class AppDomainTypeFinder
+    {
+        /// <summary>
+        /// Get the list of assemblies
+        /// </summary>
+        /// <returns></returns>
+        public static IList<Assembly> GetAssemblies()
+        {
+            var addedAssemblyNames = new List<string>();
+            var assemblies = new List<Assembly>();
+            AddAssembliesInAppDomain(addedAssemblyNames, assemblies);
+            return assemblies;
+        }
+
+        /// <summary>
+        /// Add the configured assemblies
+        /// </summary>
+        /// <param name="addedAssemblyNames"></param>
+        /// <param name="assemblies"></param>
+        public static void AddAssembliesInAppDomain(List<string> addedAssemblyNames, List<Assembly> assemblies)
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                if (!addedAssemblyNames.Contains(assembly.FullName))
+                {
+                    assemblies.Add(assembly);
+                    addedAssemblyNames.Add(assembly.FullName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Find the classes of a particular type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="onlyConcreteClasses"></param>
+        /// <returns></returns>
+        public IEnumerable<Type> FindClassesOfType<T>(bool onlyConcreteClasses = true)
+        {
+            return FindClassesOfType(typeof(T), onlyConcreteClasses);
+        }
+
+        /// <summary>
+        /// Find the classes of a particular type
+        /// </summary>
+        /// <param name="assignTypeFrom"></param>
+        /// <param name="onlyConcreteClasses"></param>
+        /// <returns></returns>
+        public IEnumerable<Type> FindClassesOfType(Type assignTypeFrom, bool onlyConcreteClasses = true)
+        {
+            return FindClassesOfType(assignTypeFrom, GetAssemblies(), onlyConcreteClasses);
+        }
+
+        /// <summary>
+        /// Find the classes of a particular type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="assemblies"></param>
+        /// <param name="onlyConcreteClasses"></param>
+        /// <returns></returns>
+        public IEnumerable<Type> FindClassesOfType<T>(IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true)
+        {
+            return FindClassesOfType(typeof(T), assemblies, onlyConcreteClasses);
+        }
+
+        /// <summary>
+        /// Find the classes of a particular type
+        /// </summary>
+        /// <param name="assignTypeFrom"></param>
+        /// <param name="assemblies"></param>
+        /// <param name="onlyConcreteClasses"></param>
+        /// <returns></returns>
+        public static IEnumerable<Type> FindClassesOfType(Type assignTypeFrom, IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true)
+        {
+            var result = new List<Type>();
+            try
+            {
+                foreach (var a in assemblies)
+                {
+                    Type[] types = null;
+                    try
+                    {
+                        types = a.GetTypes();
+                    }
+                    catch
+                    {
+                    }
+                    if (types != null)
+                    {
+                        foreach (var t in types)
+                        {
+                            if (assignTypeFrom.IsAssignableFrom(t) || (assignTypeFrom.IsGenericTypeDefinition))
+                            {
+                                if (!t.IsInterface)
+                                {
+                                    if (onlyConcreteClasses)
+                                    {
+                                        if (t.IsClass && !t.IsAbstract)
+                                        {
+                                            result.Add(t);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        result.Add(t);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                var msg = string.Empty;
+                foreach (var e in ex.LoaderExceptions)
+                    msg += e.Message + Environment.NewLine;
+
+                var fail = new Exception(msg, ex);
+
+                throw fail;
+            }
+            return result;
+        }
+    }
+}
